89b5c14721d083bbaba63a423175985d154e04bc
SkremGah <antostojcevic7@gmail.com>
2024-10-17 21:01:21 +0200

    Update CustomLoader.lua

--- CustomLoader.lua
+++ CustomLoader.lua
@@ -636,7 +636,7 @@ if CheckPlace() then
 		local Rewards = Info:WaitForChild("rewards")
 		function CheckReward()												
 			local RewardType,RewardAmount
-			--[[												
+															
           	 	repeat task.wait() until Rewards:FindFirstChild(1) and Rewards:FindFirstChild(2)--Rewards[1] and Rewards[2]
             		if Rewards[2].content.icon.Image == "rbxassetid://5870325376" then
                			RewardType = "Coins"
@@ -644,8 +644,8 @@ if CheckPlace() then
               			RewardType = "Gems"
            		end
             		RewardAmount = tonumber(Rewards[2].content.textLabel.Text)
-			]]
-															
+			
+			--[[											
 			if GetPlayerState():GetAttribute("CoinsReward") then
 				RewardType = "Coins"
 				RewardAmount = GetPlayerState():GetAttribute("CoinsReward")
@@ -653,7 +653,7 @@ if CheckPlace() then
 				RewardType = "Gems"
 				RewardAmount = GetPlayerState():GetAttribute("GemsReward")
 			end
-			
+			]]
 			return {RewardType, RewardAmount}
 		end
 		StratXLibrary.SignalEndMatch = GetGameState():GetAttributeChangedSignal("GameOver"):Connect(function()
